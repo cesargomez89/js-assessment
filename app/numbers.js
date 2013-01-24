dc5490b24201df29f32fd9441155b01add53c4be
Cesar Gomez <cesargomez@tangosource.com>
2013-01-23 22:22:33 -0600

    13 solved

--- app/numbers.js
+++ app/numbers.js
@@ -3,15 +3,14 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
     },
 
     base10: function(str) {
-
+    return parseInt(str,2);
     },
 
     convertToBinary: function(num) {
-
+      return parseInt(num,2);
     },
 
     multiply: function(a, b) {
